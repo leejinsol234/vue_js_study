f25ea8845f88ebb6ce9aaa709e21f5ca808e6993
leejinsol234 <desirable7@naver.com>
2024-06-10 18:04:18 +0900

    콘솔에 manifest.json 404 오류 해결

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>header</div>
+    <h1>TODO it!</h1>
 </template>
 
 <script>
@@ -9,4 +9,25 @@
 </script>
 
 <style>
+    body {
+        text-align: center;
+        background-color: #F6F6F8;
+    }
+    input {
+        border-style: groove;
+        width: 200px;
+    }
+    button {
+        border-style: groove;
+    }
+    .shadow {
+        box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+    }
+</style>
+<style scoped>
+    h1 {
+        color: #2f3b52;
+        font-weight: 900;
+        margin: 2.5rem 0 1.5rem;
+    }
 </style>
