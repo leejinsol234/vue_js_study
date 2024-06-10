d7daadcd6d5f060d43a1c9e10e1b54ea8ef1209b
leejinsol234 <desirable7@naver.com>
2024-06-10 17:13:31 +0900

    실전 애플리케이션 만들기

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/todoHeader.vue';
+import TodoInput from './components/todoInput.vue';
+import TodoList from './components/todoList.vue';
+import TodoFooter from './components/todoFooter.vue';
+
+  export default {
+    components: {
+      'TodoHeader': TodoHeader,
+      'TodoInput': TodoInput,
+      'TodoList': TodoList,
+      'TodoFooter': TodoFooter
+    }
+  }
+</script>
+
+<style>
+</style>
