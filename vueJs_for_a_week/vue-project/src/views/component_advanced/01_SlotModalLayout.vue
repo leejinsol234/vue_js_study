982fcaff795d0c159ffa75c708d74140d9c61767
leejinsol234 <desirable7@naver.com>
2024-06-17 09:52:07 +0900

    slot, provide와 inject

---
+++ vueJs_for_a_week/vue-project/src/views/component_advanced/01_SlotModalLayout.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="modal-container">
+        <header>
+            <!--slot에 name을 지정해서 사용하는 slot을 Named Slot이라고 힌디.
+            컴포넌트 내에 여러 영역에서 slot을 적용할 때는 name을 이용해서 적용한다.-->
+            <slot name="header"></slot>
+        </header>
+        <main>
+            <slot></slot>
+        </main>
+        <footer>
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+.modal-container {
+    border: 1px solid #ddd;
+}
+</style>
---
+++ vueJs_for_a_week/vue-project/src/views/component_advanced/03_ProvideInject.vue
@@ -0,0 +1,20 @@
+<template>
+    <ProvideInjectChild />
+</template>
+
+<script>
+import ProvideInjectChild from './04_ProvideInjectChild';
+export default {
+    components: { ProvideInjectChild },
+    data() {
+        return {
+            items: ['A', 'B'],
+        };
+    },
+    provide() {
+        return {
+            itemLength: this.items.length,
+        };
+    },
+};
+</script>
